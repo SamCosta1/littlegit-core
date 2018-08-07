0cecb09abc642ef462be3c50e33cd53822dbbfe1
Sam <samuel.dacosta@student.manchester.ac.uk>
2018-08-07 16:26:28 +0100

    Specify charset on results

--- src/main/kotlin/org/littlegit/core/shell/ShellRunnerLocal.kt
+++ src/main/kotlin/org/littlegit/core/shell/ShellRunnerLocal.kt
@@ -3,6 +3,7 @@ package org.littlegit.core.shell
 import java.io.BufferedReader
 import java.io.File
 import java.io.InputStreamReader
+import java.nio.charset.Charset
 
 class ShellRunnerLocal(private val basePath: String): ShellRunner {
 
@@ -14,8 +15,8 @@ class ShellRunnerLocal(private val basePath: String): ShellRunner {
 
         val proc = pb.start()
 
-        val stdInput = BufferedReader(InputStreamReader(proc.inputStream))
-        val stdError = BufferedReader(InputStreamReader(proc.errorStream))
+        val stdInput = BufferedReader(InputStreamReader(proc.inputStream, Charset.forName("UTF-8")))
+        val stdError = BufferedReader(InputStreamReader(proc.errorStream, Charset.forName("UTF-8")))
 
         val lines = ArrayList<String>()
         var line : String?
